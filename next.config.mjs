af6bdce29d3f6ee887784e2f2bc2cf1f2f52a0a8
pakhlavonovv <abdullokhpakhlavonov@gmail.com>
2025-03-06 13:41:00 +0500

    not-found-fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.ibb.co'], 
+    domains: ['i.ibb.co'],
   },
-  output: "standalone",
-  experimental: {
-    appDir: true, 
-  },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
