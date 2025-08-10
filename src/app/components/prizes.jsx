dfed88ada2db098a00f3f927826c1b3a7456f120
pakhlavonovv <abdullokhpakhlavonov@gmail.com>
2025-08-10 18:29:40 +0500

    changed

--- src/app/components/prizes.jsx
+++ src/app/components/prizes.jsx
@@ -3,7 +3,7 @@ import './style.css'
 const Prizes = () => {
   return (
     <div className='container flex items-center justify-center text-white'>
-    <div className='w-[95%] md:w-[85%] border-[1px] border-green-400 rounded-md p-6 flex flex-col gap-2 items-center justify-center'>
+    <div className='w-[95%] md:w-[85%] border-[3px] border-green-400 rounded-md p-6 flex flex-col gap-2 items-center justify-center'>
     <strong className='text-[30px] text-center text-black'>Призы SevenStrike</strong>
     <p className='text-[20px]'>1 место: <span className='text-green-400 underline'>1 000 000 uzs</span></p>
     <p className='text-[18px]'>2 место: <span className='text-green-400 underline'>700 000 uzs</span></p>
